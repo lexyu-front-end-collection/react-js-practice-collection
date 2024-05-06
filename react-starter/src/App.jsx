27c48c81586bf7b9fb26ec16cde9fe783fe202b0
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-05-06 21:45:08 +0800

    update

---
+++ react-starter/src/App.jsx
@@ -0,0 +1,12 @@
+import Hello from "./components/Hello";
+
+function App() {
+  return (
+    <>
+      <Hello name="ABC" msg="Here" emoji="💥" />
+      <Hello name="XYZ" msg="Nothing" emoji="🎇" />
+    </>
+  );
+}
+
+export default App;
---
+++ react-starter/src/components/Hello.jsx
@@ -0,0 +1,23 @@
+const test = "Tester";
+
+function displayMsg() {
+  return "WoW!";
+}
+
+function Hello({ name, msg, emoji }) {
+  // console.log(props);
+  // const { name, msg } = props;
+  // console.log(name, msg);
+  return (
+    <div>
+      <h1>
+        {/* Hello {props.name}, {props.msg}! {test} , {10 + 20}, {displayMsg()} */}
+        Hello {name}, {msg}
+        {emoji}! {test} , {10 + 20}, {displayMsg()}
+      </h1>
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default Hello;
