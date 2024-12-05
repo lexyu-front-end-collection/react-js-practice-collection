4d1f3ca7ba03f114723748e6a916fa8738591411
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-12-05 17:40:08 +0800

    update

--- react-starter-props/src/App.jsx
+++ react-starter-props/src/App.jsx
@@ -1,10 +1,10 @@
-
+import Student from './Student.jsx'
 
 function App() {
 
   return (
     <>
-
+      <Student name="L"></Student>
     </>
   )
 }
---
+++ react-starter-props/src/Student.jsx
@@ -0,0 +1,10 @@
+function Student(props) {
+    return(
+        <div>
+            <p>Name: {props.name}</p>
+        </div>
+    )
+}
+
+
+export default Student
