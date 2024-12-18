3660d405bb353d14fdb885e3aee0df77d6adbe1a
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-12-18 23:33:23 +0800

    update

--- react-ts-tailwind-demo/src/App.tsx
+++ react-ts-tailwind-demo/src/App.tsx
@@ -1,8 +1,19 @@
+import { dummyData } from "./data/todos"
+
 function App() {
 
   return (
-    <div className="py-20 h-screen">
-      <h1 className="font-bold text-3xl text-center">Setup Demo(Todos)</h1>
+    <div className="py-10 h-screen">
+      <h1 className="text-3xl font-bold text-center">Setup Demo(Todos)</h1>
+      <div className="mx-auto max-w-lg">
+        <div className="space-y-2">
+          {dummyData.map((item) => (
+            <p key={item.id} className="text-lg">
+              <p>{item.title} - {String(item.completed)}</p>
+            </p>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
--- react-ts-tailwind-demo/src/data/todos.ts
+++ react-ts-tailwind-demo/src/data/todos.ts
@@ -1,12 +1,14 @@
-export const dummyData = [
-    { id: 1, title: 'Buy groceries', completed: false },
-    { id: 2, title: 'Walk the dog', completed: true },
-    { id: 3, title: 'Finish project report', completed: false },
-    { id: 4, title: 'Read a book', completed: true },
-    { id: 5, title: 'Exercise', completed: false },
-    { id: 6, title: 'Call mom', completed: true },
-    { id: 7, title: 'Plan vacation', completed: false },
-    { id: 8, title: 'Clean the house', completed: true },
-    { id: 9, title: 'Attend meeting', completed: false },
-    { id: 10, title: 'Buy a gift', completed: true }
+import { Todo } from '../types/todo'
+
+export const dummyData: Todo[] = [
+    { id: 1, title: 'Buy groceries 🎂👍', completed: false },
+    { id: 2, title: 'Walk the dog 🐕💦', completed: true },
+    { id: 3, title: 'Finish project report 📄📊', completed: false },
+    { id: 4, title: 'Read a book 📚👓', completed: true },
+    { id: 5, title: 'Exercise 🏋️‍♂️💪', completed: false },
+    { id: 6, title: 'Call mom 📞👵', completed: true },
+    { id: 7, title: 'Plan vacation 🏖️🗺️', completed: false },
+    { id: 8, title: 'Clean the house 🧹💪', completed: true },
+    { id: 9, title: 'Attend meeting 🗓️📝', completed: false },
+    { id: 10, title: 'Buy a gift 🎁🎁', completed: true }
 ];
