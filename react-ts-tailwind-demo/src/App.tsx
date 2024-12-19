ae79b54fb3d0abce38c08229450bf86704862951
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-12-19 22:58:31 +0800

    update

--- react-ts-tailwind-demo/src/App.tsx
+++ react-ts-tailwind-demo/src/App.tsx
@@ -1,22 +1,37 @@
 import { dummyData } from "./data/todos"
 import TodoItem from "./components/TodoItem"
+import { useState } from "react"
+import { Todo } from "./types/todo"
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>(dummyData)
+
+  function setTodoCompleted(id: number, completed: boolean) {
+    // alert(`id: ${id}, completed: ${completed}`)
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed } : todo
+      )
+    )
+  }
 
   return (
-    <div className="py-10 h-screen">
+    <main className="py-10 space-y-5 h-screen">
       <h1 className="text-3xl font-bold text-center">Setup Demo(Todos)</h1>
-      <div className="mx-auto max-w-lg">
+      <div className="p-5 mx-auto max-w-lg rounded-md bg-slate-100">
         <div className="space-y-2">
-          {dummyData.map((todo) => (
-            <TodoItem todo={todo} />
+          {todos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onCompletedChange={setTodoCompleted} />
             // <p key={item.id} className="text-lg">
             //   <p>{item.title} - {String(item.completed)}</p>
             // </p>
           ))}
         </div>
       </div>
-    </div>
+    </main>
   )
 }
 
--- react-ts-tailwind-demo/src/components/TodoItem.tsx
+++ react-ts-tailwind-demo/src/components/TodoItem.tsx
@@ -2,14 +2,15 @@ import { Todo } from "../types/todo";
 
 interface TodoItemProps {
     todo: Todo
+    onCompletedChange: (id: number, completed: boolean) => void
 }
 
-export default function TodoItem({ todo }: TodoItemProps) {
+export default function TodoItem({ todo, onCompletedChange }: TodoItemProps) {
 
     return (
         <div className="">
-            <label key={todo.id} className="flex items-center border gap-7 rounded-md p-2 border-gray-400 bg-white hover:bg-slate-50">
-                <input type="checkbox" className="scale-125" />
+            <label key={todo.id} className="flex gap-7 items-center p-2 bg-white rounded-md border border-gray-400 hover:bg-slate-50">
+                <input type="checkbox" className={"scale-125"} checked={todo.completed} onChange={(e) => onCompletedChange(todo.id, e.target.checked)} />
                 <span className={todo.completed ? "line-through text-gray-400" : ""}>
                     {todo.title}
                 </span>
